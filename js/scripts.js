298f24f7b3d01cafdfa5e6ab06350c17f1331858
Student <example@example.com>
2019-10-24 16:38:08 -0700

    finally got something to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
   $("#charecterButton").click(function(event) {
-    var foodAnswer = $("#food").val()
-    var movieAnswer = $("#movie").val()
-    var colorAnswer = $("color")
+    var foodAnswer = parseInt($("#food").val())
+    var movieAnswer = parseInt($("#movie").val())
+    var colorAnswer = parseInt($("#color").val())
+    var timeAnswer = parseInt($("#time").val())
+
+    var totalValue = foodAnswer + movieAnswer + colorAnswer + timeAnswer;
+
+
+
+
+    if (totalValue <= 5 ){
+      prompt("hi")
+
+    }
 
     event.preventDefault();
   });
