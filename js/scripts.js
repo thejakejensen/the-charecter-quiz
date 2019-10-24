1e7db0e9c7ca7b10c0f1e804f9f6d21a9a3842ca
Student <example@example.com>
2019-10-24 16:22:53 -0700

    add back end logic

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#charecterButton").click(function(event) {
+    var foodAnswer = $("#food").val()
+    var movieAnswer = $("#movie").val()
+    var colorAnswer = $("color")
+
+    event.preventDefault();
+  });
+});
